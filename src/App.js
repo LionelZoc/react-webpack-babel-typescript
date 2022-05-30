99fdab542421c76f60e75a8a7fa646365d69f413
lionel zoc <lionel@wisembly.com>
2022-05-31 01:18:24 +0200

    add typescript loader in webpack

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-//import Test from "./components/mine";
+import Test from "./components/mine";
 import { again } from "./components/other";
 function App() {
   return (
     <div>
-      {/*<Test value={3} />*/}
+      <Test value={3} />
       <h2>Welcome to Amir Muerte</h2>
       <h3>Date : {new Date().toDateString()}</h3>
     </div>
--- webpack.config.js
+++ webpack.config.js
@@ -26,13 +26,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-react"],
-          },
+          // options: {
+          //   presets: ["@babel/preset-react"],
+          // },
         },
       },
       {
